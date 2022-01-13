6267969c6d678f8c9d5453300dcbcc722b160c9e
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:33:57 +0530

    Specified Operations

---
+++ Specified_operations.java
@@ -0,0 +1,13 @@
+
+public class Specified_operations {
+	public static void main(String[] args) {
+		int num1=10+13;
+		System.out.println("Result of 1st expression is : " + num1);
+		num1=15-10;
+		System.out.println("Result of 2nd expression is : " + num1);
+		num1=3*4;
+		System.out.println("Result of 3rd expression is : " + num1);
+		num1=18/6;
+		System.out.println("Result of 4th expression is : " + num1);
+	}
+}
