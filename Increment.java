04ff3688b6421513527fe249691046b439756671
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:33:26 +0530

    Increment

---
+++ Increment.java
@@ -0,0 +1,12 @@
+
+public class Increment {
+	public static void main(String[] args) {
+		int num=57;
+		System.out.println("post increment by 1 the number is : " + ++num);
+		System.out.println("pre increment by 1 the number is : " + ++num);
+		System.out.println("increasing by 1 the number is : " + ++num);
+		System.out.println("post decrement by 1 the number is : " + --num);
+		System.out.println("pre decrement by 1 the number is : " + --num);
+		System.out.println("decreasing by 1 the number is : " + --num);
+	}
+}
