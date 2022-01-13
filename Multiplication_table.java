2fcd6bd1d5d5e9276d80ee316fd814f08f73987c
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:19:55 +0530

    Multiplication table

---
+++ Multiplication_table.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class Multiplication_table {
+	public static void main(String args[]) {
+		int num=5,i,num1;
+	    for(i=1;i<=10;i++){
+	        num1=num*i;
+	        System.out.println(num1);
+	    }
+	}
+}
