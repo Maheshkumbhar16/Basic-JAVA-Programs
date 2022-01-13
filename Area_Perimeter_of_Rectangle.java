6bd37a9bd45c2e46023a56c159c7a1dcb90c0f88
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:11:52 +0530

    Area and Perimeter of Rectangle

---
+++ Area_Perimeter_of_Rectangle.java
@@ -0,0 +1,13 @@
+package examples;
+
+public class Area_Perimeter_of_Rectangle {
+	public static void main(String args[]) {
+		int l=10,b=15, periofreact, areaofreact;
+	    areaofreact = l*b;
+	    periofreact = 2*(l+b);
+	    System.out.println("Lenght of reactangle is : " + l);
+	    System.out.println("Width of reactangle is : " + b);
+	    System.out.println("Perimeter of reactangle is : " + periofreact);
+	    System.out.println("Area of reactangle is : " + areaofreact);
+	}
+}
