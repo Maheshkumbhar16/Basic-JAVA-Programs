251d89645d7bf686efe6d56fa963410a1f69c966
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:12:37 +0530

    Celsius to Fahrenheit

---
+++ Celsius_to_Fahrenheit.java
@@ -0,0 +1,12 @@
+package examples;
+
+public class Celsius_to_Fahrenheit {
+	public static void main(String args[]) {
+		//formula is F=C*1.8+32
+		int c=35;
+		double f;
+		f=c*1.8+32;
+		System.out.println("Temp in Celsius is : " + c);
+		System.out.println("Temp in Fahrenheit is : " + f);
+	}
+}
