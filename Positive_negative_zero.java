95d0c1df6845e19e33790998364abe41b910a062
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:21:09 +0530

    Positive, Negative and Zero
    Verify the enter number is positive, negative and zero

---
+++ Positive_negative_zero.java
@@ -0,0 +1,15 @@
+package examples;
+
+public class Positive_negative_zero {
+	public static void main(String args[]) {
+		double num=-60;
+	    if(num==0){
+	        System.out.println("Number is zero");
+	    }
+	    else if(num>=0){
+	    	System.out.println("Number is positive");
+	    }
+	    else
+	    	System.out.println("Number is negative");
+	}
+}
