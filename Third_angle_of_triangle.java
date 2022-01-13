98e70d2371b65e07e6e71f71b442fdd1e9adba45
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:24:14 +0530

    Third angle of triangle

---
+++ Third_angle_of_triangle.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class Third_angle_of_triangle {
+	public static void main(String args[]) {
+		int a=30,b=60,c;
+	    c=180-a-b;
+	    System.out.println("First angle of triangle is : " + a);
+	    System.out.println("Second angle of triangle is : " + b);
+	    System.out.println("Third angle of triangle is : " + c);
+	}
+}
