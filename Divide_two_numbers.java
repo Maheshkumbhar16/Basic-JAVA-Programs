e7150709a1f53942b05b0186d9e89dd6165b3f67
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:14:56 +0530

    Divide two numbers

---
+++ Divide_two_numbers.java
@@ -0,0 +1,9 @@
+package examples;
+
+public class Divide_two_numbers {
+	public static void main(String args[]) {
+		int num1=30, num2=10;
+	    System.out.println("Enter numbers are : " + num1 + "/n" + num2);
+	    System.out.println("Quotient of " + num1 + " and " + num2 + " is : " + num1/num2);
+	}
+}
