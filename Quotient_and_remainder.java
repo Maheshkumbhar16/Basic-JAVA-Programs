0a6191fd100c75de9fb2072d1e72a9eab46feb44
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:21:50 +0530

    Quotient and Remainder

---
+++ Quotient_and_remainder.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class Quotient_and_remainder {
+	public static void main(String args[]) {
+		int x=25, y=3;
+	    System.out.println("First number is : " + x);
+	    System.out.println("Second number is : " + y);
+	    System.out.println("The quotient of the divison is : " + x/y);
+	    System.out.println("The remainder of the division is : " + x%y);
+	}
+}
