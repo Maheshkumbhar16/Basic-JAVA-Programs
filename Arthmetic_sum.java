6ae0da0b114eb202186035f1dba348f973ef506c
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:32:59 +0530

    Arithmetic sum

---
+++ Arthmetic_sum.java
@@ -0,0 +1,31 @@
+
+public class Arthmetic_sum {
+	public static void main(String[] args) {
+		int a=5, b=7;
+		double c=3.7, d=8.0;
+		int ans= a+b;
+		System.out.println("Sum of a and b : " + ans);
+		double ans1= c+d;
+		System.out.println("Sum of a and b : " + ans1);
+		ans1= a+d;
+		System.out.println("Sum of a and b : " + ans1);
+		ans= a-b;
+		System.out.println("Sum of a and b : " + ans);
+		ans1= c-d;
+		System.out.println("Sum of a and b : " + ans1);
+		ans1= a-d;
+		System.out.println("Sum of a and b : " + ans1);
+		ans= a*b;
+		System.out.println("Sum of a and b : " + ans);
+		ans1= c*d;
+		System.out.println("Sum of a and b : " + ans1);
+		ans1= a*d;
+		System.out.println("Sum of a and b : " + ans1);
+		ans= a/b;
+		System.out.println("Sum of a and b : " + ans);
+		ans1= c/d;
+		System.out.println("Sum of a and b : " + ans1);
+		ans1= a/d;
+		System.out.println("Sum of a and b : " + ans1);
+	}
+}
