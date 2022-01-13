c3e2ceb329dab06e9f85adadd4c5e1cb3c26a110
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:18:36 +0530

    kph to mph
    Kilometer per hour to miles per hour

---
+++ kph_to_mph.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class kph_to_mph {
+	public static void main(String args[]) {
+		//m=k/1.609344
+		double k=25,m;
+		m=k/1.609344;
+		System.out.println("Speed in kilometer per hour is : " + k);
+		System.out.println("Speed in miles in per hour is : " + m);
+	}
+}
