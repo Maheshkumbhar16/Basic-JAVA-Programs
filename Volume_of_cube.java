2e36c6ce2630a80572b800b2c6f87629028c012f
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:25:26 +0530

    Volume of cube

---
+++ Volume_of_cube.java
@@ -0,0 +1,10 @@
+package examples;
+
+public class Volume_of_cube {
+	public static void main(String args[]) {
+		int side=5;
+		int vol_cube=side*side*side;
+		System.out.println("Side of cube is : " + side);
+		System.out.println("Volume of cube is : " + vol_cube);
+	}
+}
