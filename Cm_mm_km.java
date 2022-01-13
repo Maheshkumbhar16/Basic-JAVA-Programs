e0899201b20c2748c2b3e8ca6d5d88349c3d4345
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:14:13 +0530

    Centimeter to millimeter and kilometer

---
+++ Cm_mm_km.java
@@ -0,0 +1,10 @@
+package examples;
+
+public class Cm_mm_km {
+	public static void main(String args[]) {
+		double num=10000;
+	    System.out.println("Length in centimeter is : " + num);
+	    System.out.println("Length in meter is : " + (num/100));
+	    System.out.println("Length in kilometer is : " + (num/1000));
+	}
+}
