7da4da6f243dd6433750d9ab4150a843882397b9
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:24:52 +0530

    Total and average

---
+++ Total_and_average.java
@@ -0,0 +1,9 @@
+package examples;
+
+public class Total_and_average {
+	public static void main(String args[]) {
+		   double num1=10, num2=15, num3=30, num4=40;
+		   System.out.println("Addition is : " + (num1+num2+num3+num4));
+		   System.out.println("Average is : "  + ((num1+num2+num3+num4)/4));
+	}
+}
