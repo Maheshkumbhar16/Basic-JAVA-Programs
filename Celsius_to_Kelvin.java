56734ea953650df4b89df542354a47c4f6daa652
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:13:18 +0530

    Celsius to Kelvin

---
+++ Celsius_to_Kelvin.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class Celsius_to_Kelvin {
+	public static void main(String args[]) {
+		//formula is k=c+273.15
+		double c=26.85, k;
+		k=c+273.15;
+		System.out.println("Temp in Celsius is : " + c);
+		System.out.println("Temp in Kelvin is : " + k);
+	}
+}
