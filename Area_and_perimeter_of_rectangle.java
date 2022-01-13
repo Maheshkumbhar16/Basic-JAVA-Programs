270b9921db5bf4fff2b4492fe848f5c448906b7a
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:08:43 +0530

    Area and perimeter of Rectangle
    Area and perimeter of Rectangle

---
+++ Area_and_perimeter_of_rectangle.java
@@ -0,0 +1,11 @@
+package examples;
+
+public class Area_and_perimeter_of_rectangle {
+	public static void main(String args[]) {
+		double leng=8.5, brdth=5.6;
+	    System.out.println("Width of the rectangle is : " + leng);
+	    System.out.println("Height of the rectangle is : " + brdth);
+	    System.out.println("Area of the rectangle is : " + (leng*brdth));
+	    System.out.println("Perimeter of the rectangle is : "  + (2*(leng+brdth)));
+	}
+}
