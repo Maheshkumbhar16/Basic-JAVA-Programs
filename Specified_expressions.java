26daad347739b66f9a4029315df3924c4beab578
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:22:25 +0530

    Specified expressions

---
+++ Specified_expressions.java
@@ -0,0 +1,9 @@
+package examples;
+
+public class Specified_expressions {
+	public static void main(String args[]) {
+		double num;
+	    num=((25.5*3.5)-(3.5*3.5))/(40.5-4.5);
+	    System.out.println(num);
+	}
+}
