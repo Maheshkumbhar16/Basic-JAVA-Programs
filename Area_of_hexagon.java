24bd6fcbabdad86afd7eb3c633003176a45fd2fa
Mahesh Kumbhar <93268828+Maheshkumbhar16@users.noreply.github.com>
2022-01-13 22:10:42 +0530

    Area of Hexagon

---
+++ Area_of_hexagon.java
@@ -0,0 +1,9 @@
+package examples;
+
+public class Area_of_hexagon {
+	public static void main(String args[]) {
+		int a=6;
+	    System.out.println("Side of the hexagon : " + a);
+	    System.out.println("Area of the hexagon : " + ((3*a*a*sqrt(3))/2));
+	}
+}
